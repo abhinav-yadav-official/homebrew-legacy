13916c3e44ff7a9a5a2481a1534bb7a5dd165f09
Abhinav Yadav <abhi.ay.in@gmail.com>
2025-05-04 20:08:40 +0530

    Add legacy libxml2 and libxslt

---
+++ Formula/libxml2.rb
@@ -0,0 +1,114 @@
+class Libxml2 < Formula
+  desc "GNOME XML library"
+  homepage "http://xmlsoft.org/"
+  url "https://download.gnome.org/sources/libxml2/2.11/libxml2-2.11.4.tar.xz"
+  sha256 "737e1d7f8ab3f139729ca13a2494fd17bf30ddb4b7a427cf336252cab57f57f7"
+  license "MIT"
+
+  # We use a common regex because libxml2 doesn't use GNOME's "even-numbered
+  # minor is stable" version scheme.
+  livecheck do
+    url :stable
+    regex(/libxml2[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "6c0efb5d6ae778818b4d6f854c77223f3cfd1bbc2c1612a8b46788473fe37f28"
+    sha256 cellar: :any,                 arm64_monterey: "1667e64e783617e6547b81646373dc73d40fdc5bccd8e280aab8af867d2d0053"
+    sha256 cellar: :any,                 arm64_big_sur:  "912797049c1432380d4a404c955e0279085d36a903111d94e95283daf11ed4e0"
+    sha256 cellar: :any,                 ventura:        "b87db325940e4116e1f74e88698b1b09115a0d65016e5dca4723a7c4316c844d"
+    sha256 cellar: :any,                 monterey:       "df2f13c321754b4648d8c76747df838fee338057403790eb229eb00e5c373c03"
+    sha256 cellar: :any,                 big_sur:        "920721a8b825d49be9d29a51dd56d51e592f5d7eb817ddfb1f7572d82ae7d706"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7152f147ee801b49bea2b241981705b7789e22964ad7f8a7301e8f1d214c9ae9"
+  end
+
+  head do
+    url "https://gitlab.gnome.org/GNOME/libxml2.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
+  end
+
+  keg_only :provided_by_macos
+
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+  depends_on "pkg-config" => :test
+  depends_on "icu4c"
+  depends_on "readline"
+
+  uses_from_macos "zlib"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    system "./configure", *std_configure_args,
+                          "--disable-silent-rules",
+                          "--with-history",
+                          "--with-icu",
+                          "--without-python",
+                          "--without-lzma"
+    system "make", "install"
+
+    cd "python" do
+      sdk_include = if OS.mac?
+        sdk = MacOS.sdk_path_if_needed
+        sdk/"usr/include" if sdk
+      else
+        HOMEBREW_PREFIX/"include"
+      end
+
+      includes = [include, sdk_include].compact.map do |inc|
+        "'#{inc}',"
+      end.join(" ")
+
+      # We need to insert our include dir first
+      inreplace "setup.py", "includes_dir = [",
+                            "includes_dir = [#{includes}"
+
+      pythons.each do |python|
+        system python, *Language::Python.setup_install_args(prefix, python)
+      end
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <libxml/tree.h>
+
+      int main()
+      {
+        xmlDocPtr doc = xmlNewDoc(BAD_CAST "1.0");
+        xmlNodePtr root_node = xmlNewNode(NULL, BAD_CAST "root");
+        xmlDocSetRootElement(doc, root_node);
+        xmlFreeDoc(doc);
+        return 0;
+      }
+    EOS
+
+    # Test build with xml2-config
+    args = shell_output("#{bin}/xml2-config --cflags --libs").split
+    system ENV.cc, "test.c", "-o", "test", *args
+    system "./test"
+
+    # Test build with pkg-config
+    ENV.append "PKG_CONFIG_PATH", lib/"pkgconfig"
+    args = shell_output("#{Formula["pkg-config"].opt_bin}/pkg-config --cflags --libs libxml-2.0").split
+    system ENV.cc, "test.c", "-o", "test", *args
+    system "./test"
+
+    pythons.each do |python|
+      with_env(PYTHONPATH: prefix/Language::Python.site_packages(python)) do
+        system python, "-c", "import libxml2"
+      end
+    end
+  end
+end
